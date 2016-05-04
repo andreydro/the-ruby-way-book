00287222f203196139e49119221e7bd4d9cea21d
dro14 <and-rey-2309@mail.ru>
2016-05-04 22:27:25 +0300

    Add 1 chapter

---
+++ 1_chapter.rb
@@ -0,0 +1,25 @@
+[1, 2, 3]
+[1, 2, "boots"]
+[1, 2, [3, 4], 5]
+["alpha", "beta", "gamma", "delta"]
+
+%w[alpha beta gamma delta]
+%w[ Jan Fev Mar Apr May Jun Jul Aug Sep Oct Nov Dec]
+%w/am is are was were be being been/
+
+val = myarray[0]
+print stats[j]
+x[i] = x[i + 1]
+
+{1 => 1, 2 => 4, 3 => 9, 4 => 16, 5 => 25, 6 => 36}
+{"cat" => "cats", "ox" => "oxen", "bacterium"=> "bacteria"}
+{"adds" => [1,3,5,7], "evens" => [2,4,6,8]}
+{"foo" => 123, [4,5,6] => "my array", "867-5309" => "Jenny"}
+
+{hydrogen: 1, helium: 2, carbon: 12}
+
+print phone_numbers["Jenny"]
+plurals["octopus"] = "octopi"
+atomic_numers[:helium] # -> 2
+
+
